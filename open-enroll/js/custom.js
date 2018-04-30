0bfc117ee479e57feda004a5df443e0396c58788
David <davidkrasniy@gmail.com>
2018-04-30 08:27:19 -0700

    register

--- open-enroll/js/custom.js
+++ open-enroll/js/custom.js
@@ -1,11 +1,5 @@
 (function($) {
 
-   var stickyBottomNavHeight = $('.bottom-nav').height();
-
-
-    $('body').css('padding-bottom', (stickyBottomNavHeight+20)+'px');
-
-    console.log(stickyBottomNavHeight);
 
     // FLoating labels
     $("#loginUsername, #inputPassword").bind('input propertychange', function () {
@@ -29,6 +23,40 @@
         $('.contact-us-section').removeClass('open');
     });
 
+    var bottomFixedNavHeight = $('.bottom-nav').height();
+
+    var cardActionHeight = $('.card-action').height();
+    $('.card-action').css('padding-bottom', (bottomFixedNavHeight+15)+'px');
+
+    //if null set to 0
+    if (!bottomFixedNavHeight) {
+        bottomFixedNavHeight = 0;
+    }
+    if (!cardActionHeight) {
+        cardActionHeight = 0;
+    }
+    var bottomPaddingToAdd = bottomFixedNavHeight + cardActionHeight;
+
+    console.log(bottomPaddingToAdd);
+    $('body').css('padding-bottom', (bottomPaddingToAdd+20)+'px');
+
+
+
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+    // Get the device pixel ratio
+    var ratio = window.devicePixelRatio || 1;
+
+    // Define the users device screen dimensions
+    var screen = {
+        width : window.screen.width * ratio,
+        height : window.screen.height * ratio
+    };
+
+    // iPhone X Detection
+    if (iOS && screen.width == 1125 && screen.height === 2436) {
+        alert('iPhoneX Detected!');
+    }
 
 
 })(jQuery);
