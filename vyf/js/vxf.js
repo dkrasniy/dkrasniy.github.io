61fa2816db99ee1fa071c9f7e8d7403e7a663c1b
David Krasniy <davidkrasniy@gmail.com>
2019-02-25 07:18:41 -0800

    vxf

--- vyf/js/vxf.js
+++ vyf/js/vxf.js
@@ -8,3 +8,22 @@ $("#loginUsername, #loginPassword").bind('input propertychange', function () {
         $(this).prev().addClass('float-true');
     }
 });
+
+$('.syncDocumentName').keyup(function() {
+    var ID = this.getAttribute('id')
+    var dInput = this.value;
+    $('.document-title-'+ID).html(dInput)
+   
+    if( $(this).val().length === 0 ) {
+        $('.document-title-'+ID).html('No document name')
+        $('.document-title-'+ID).addClass('missing-document-name')
+    }else {
+        $('.document-title-'+ID).removeClass('missing-document-name')
+    }
+    
+});
+
+$('.toggle-route-slip').click(function() {
+    $('.routing-slip').toggleClass('toggle-route-slip')
+    $('.folder-view').toggleClass('route-slip-collapsed')
+})
